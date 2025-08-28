741477c154cb26b14e80d9ba31ba4d60e0f09092
bhardwajjyash <demousehack@gmail.com>
2025-08-28 17:21:26 +0530

    new question added

---
+++ coursesheduler1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        int n = adj.size();
+        queue<int> q;
+        vector<int> indegree(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        int cnt = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        if (cnt == n)
+            return true;
+        return false;
+    }
+};
