1044716b8840facd0c02b3acb6650c711d234fb4
bhardwajjyash <demousehack@gmail.com>
2025-09-14 15:15:42 +0530

    Added new Question

---
+++ min-multiplication-to-reach-end.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        vector<int> dist(100000 ,1e9);
+        queue<pair<int,int>> q;
+        q.push({start,0});
+        int mod = 100000 ;
+        dist[start] = 0;
+        if (start == end) return 0;
+        while(!q.empty()){
+            auto it = q.front();
+            int node = it.first;
+            int steps = it.second;
+            q.pop();
+            for(auto iter : arr){
+                int num = (iter * node)%mod;
+                if(steps+1<dist[num]){
+                    dist[num] = steps+1;
+                
+                    if(num == end){
+                        return steps+1;
+                    }
+                    q.push({num,steps+1});
+                    
+                }
+            }
+        }
+        return -1;
+    }
+};
